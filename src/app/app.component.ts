c9520a418f5b0c45ca7274f8e762b7b640dac2a5
Abdul Khan <arkhan4@gmail.com>
2021-03-14 14:12:13 -0400

    Routing logic wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
+  test = '1'+4+5;
+  num = 1+4;
 }
